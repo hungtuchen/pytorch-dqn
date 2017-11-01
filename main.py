6e0018dff006652a1ccbb8370909f446eeedd4b7
Praveen Palanisamy <praveenofpersia@gmail.com>
2017-10-31 22:31:49 -0400

    Fixed the typo in TARGER_UPDATE_FREQ. TO solve issue #7

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ REPLAY_BUFFER_SIZE = 1000000
 LEARNING_STARTS = 50000
 LEARNING_FREQ = 4
 FRAME_HISTORY_LEN = 4
-TARGER_UPDATE_FREQ = 10000
+TARGET_UPDATE_FREQ = 10000
 LEARNING_RATE = 0.00025
 ALPHA = 0.95
 EPS = 0.01
@@ -43,7 +43,7 @@ def main(env, num_timesteps):
         learning_starts=LEARNING_STARTS,
         learning_freq=LEARNING_FREQ,
         frame_history_len=FRAME_HISTORY_LEN,
-        target_update_freq=TARGER_UPDATE_FREQ,
+        target_update_freq=TARGET_UPDATE_FREQ,
     )
 
 if __name__ == '__main__':
